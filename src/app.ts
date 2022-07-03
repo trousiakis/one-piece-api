56e7c1cf670f5c4ac0b9c314f634f11b97d4ee38
Thomas Rousiakis <rousiakisthom@gmail.com>
2022-07-03 18:48:04 +0300

    Set queries for each route and create a object for json response

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,44 @@
 import express, {Request, Response} from 'express';
 import mysql from 'mysql';
 
-const app =  express();
+//Create Express App
+const app =  express(); 
+//Make connection with planetscale DB
+const connectionString = process.env.DATABASE_URL || '';
+const connection = mysql.createConnection(connectionString);
+connection.connect();
 
+//2 routes, the first for all characters, second a character by id
 app.get('/api/characters', (req: Request, res:Response) => {
-    res.send("It works")
+    const query = 'SELECT * FROM Characters';
+    connection.query(query, (err, rows) => {
+        if(err) throw err;
+
+        const retVal = {
+            data: rows,
+            message: rows.length === 0 ? 'No records found': null,
+        }
+
+        return res.send(rows);
+    })
 })
 
 app.get('/api/characters/:id', (req: Request, res:Response) => {
-    const id = req.params.id
-    res.send(`It works for id ${id}`)
+    const {id} = req.params
+    const query = `SELECT * FROM Characters WHERE ID = ${id} LIMIT 1`;
+    connection.query(query, (err, rows) => {
+        if(err) throw err;
+        const retVal = {
+            data: rows.length > 0 ? rows[0] : null,
+            message: rows.length === 0 ? 'No Record Found' : null,
+        }
+        return res.send(retVal);
+    })
+   
 })
 
+
+//Start listening to port 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("App is running")
